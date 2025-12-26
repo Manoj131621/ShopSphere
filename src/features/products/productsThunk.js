9ba1a4dad38d5938e29c45bdbd30abc2bd4b0ea2
Manojkumar <manojprfp@gmail.com>
2025-12-27 04:35:35 +0530

    feat: add product details page with dynamic routing and single product fetch

--- src/features/products/productsThunk.js
+++ src/features/products/productsThunk.js
@@ -1,10 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchAllProducts } from "../../api/productAPI";
+import { fetchAllProducts, fetchProductById } from "../../api/productAPI";
 
 export const getProducts = createAsyncThunk('products/getProducts',
     async(_,{ rejectWithValue }) => {
         try{
             return await fetchAllProducts()
+        } catch (error){
+            return rejectWithValue(error.message)
+        }
+    }
+)
+
+export const getProductById = createAsyncThunk(
+    'products/getProductById',
+    async(id, { rejectWithValue}) => {
+        try{
+            return await fetchProductById(id)
         } catch (error){
             return rejectWithValue(error.message)
         }
