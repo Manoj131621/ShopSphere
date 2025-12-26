94410548ffaec97dc623878015925936135546eb
Manojkumar <manojprfp@gmail.com>
2025-12-26 17:56:53 +0530

    feat: setup routing with lazy loading and suspense

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader(){
+    return(
+        <div style={{ textAlign:'center', marginTop:'2rem'}}>
+            Loading...
+        </div>
+    )
+}
+
+export default Loader
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,5 @@
+function CartPage(){
+    return <h1>Your Cart</h1>
+}
+
+export default CartPage
